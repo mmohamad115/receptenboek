a450dcfd81fccf6432034fb85d7b45375bbbbf3c
Mohamad Mohamad <mmohamad115@student.novacollege.nl>
2023-03-23 12:51:19 +0100

    main

--- site/index.php
+++ site/index.php
@@ -25,6 +25,9 @@ $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@200&display=swap" rel="stylesheet">
     <title>Mijn Restaurant</title>
 </head>
 <body>
